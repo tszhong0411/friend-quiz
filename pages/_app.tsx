187a031fd4f1dbff860ec5628a80d15552c2f581
小康 <lainelson411@gmail.com>
2022-03-11 10:18:14 +0800

    fix: font family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { createTheme, ThemeProvider as MUIProvider } from '@mui/material/styles'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 
@@ -6,11 +7,30 @@ import '@/styles/global.css'
 
 import Analytics from '@/components/analytics'
 
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Noto Sans TC',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+})
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme={'system'}>
-      <Analytics />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <MUIProvider theme={theme}>
+      <ThemeProvider attribute="class" defaultTheme={'system'}>
+        <Analytics />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </MUIProvider>
   )
 }
