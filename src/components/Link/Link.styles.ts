5b73d4f7d170646143289a73049340beac09a992
tszhong0411 <lainelson411@gmail.com>
2022-07-24 03:42:51 +0800

    :truck: Move Link to src folder

---
+++ src/components/Link/Link.styles.ts
@@ -0,0 +1,13 @@
+import { createStyles } from '@mantine/core'
+
+export const useStyles = createStyles(() => ({
+  externalLink: {
+    position: 'relative',
+    top: -1,
+    marginLeft: 2,
+    display: 'inline-block',
+    width: 16,
+    height: 16,
+    verticalAlign: 'middle',
+  },
+}))
---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import { Anchor, AnchorProps } from '@mantine/core'
+import Link from 'next/link'
+import React from 'react'
+import { ExternalLink } from 'tabler-icons-react'
+
+import { useStyles } from './Link.styles'
+
+const CustomLink = ({
+  href,
+  children,
+  noIcon = false,
+  ...rest
+}: React.ComponentPropsWithRef<'a'> & {
+  noIcon?: boolean
+} & AnchorProps<'a'>) => {
+  const isInternalLink = href && href.startsWith('/')
+  const isAnchorLink = href && href.startsWith('#')
+  const { classes } = useStyles()
+
+  if (isInternalLink) {
+    return (
+      <Link href={href} passHref>
+        <Anchor {...rest}>{children}</Anchor>
+      </Link>
+    )
+  }
+
+  if (isAnchorLink) {
+    return (
+      <Anchor href={href} {...rest}>
+        {children}
+      </Anchor>
+    )
+  }
+  return (
+    <Anchor target='_blank' rel='noopener noreferrer' href={href} {...rest}>
+      {children}
+      {!noIcon && (
+        <span>
+          <ExternalLink size={18} className={classes.externalLink} />
+        </span>
+      )}
+    </Anchor>
+  )
+}
+
+export default CustomLink
