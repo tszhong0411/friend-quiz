2a509bcab863780a12a3967b31e84144691f2d52
tszhong0411 <lainelson411@gmail.com>
2022-07-24 03:42:19 +0800

    :truck: Move data to src folder

---
+++ src/data/config.ts
@@ -0,0 +1,22 @@
+import { ConfigType } from 'src/lib/types'
+
+export const config: ConfigType = {
+  support_site: [
+    {
+      name: 'Holaquiz',
+      href: 'https://cn.holaquiz.com/',
+    },
+    {
+      name: 'Buddymojo',
+      href: 'https://cn.buddymojo.com/',
+    },
+    {
+      name: 'Bakequiz',
+      href: 'https://cn.bakequiz.com/',
+    },
+    {
+      name: 'Hellomate',
+      href: 'https://cn.hellomate.me/',
+    },
+  ],
+}
