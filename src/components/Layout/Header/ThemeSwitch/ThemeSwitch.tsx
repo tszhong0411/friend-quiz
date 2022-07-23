bd6982f1e3a41996cd57c719c877d047f5020f9b
tszhong0411 <lainelson411@gmail.com>
2022-07-24 03:45:19 +0800

    :sparkles: Add Language Switch

---
+++ src/components/Layout/Header/ThemeSwitch/ThemeSwitch.tsx
@@ -0,0 +1,31 @@
+import { Button, Tooltip, useMantineColorScheme } from '@mantine/core'
+import useTranslation from 'next-translate/useTranslation'
+import React from 'react'
+import { MoonStars, Sun } from 'tabler-icons-react'
+
+import { useStyles } from '@/components/Layout/Header/Header.styles'
+
+const ThemeSwitch = () => {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
+  const dark = colorScheme === 'dark'
+  const { classes } = useStyles()
+  const { t } = useTranslation()
+
+  return (
+    <Tooltip
+      label={dark ? t('common:lightmode') : t('common:darkmode')}
+      openDelay={500}
+    >
+      <Button
+        variant='filled'
+        color='gray'
+        className={classes.button}
+        onClick={() => toggleColorScheme()}
+      >
+        {dark ? <Sun size={20} /> : <MoonStars size={20} />}
+      </Button>
+    </Tooltip>
+  )
+}
+
+export default ThemeSwitch
