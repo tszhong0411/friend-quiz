dccbee0c183803bfbdbd0772612fc6a38eaa8b87
tszhong0411 <lainelson411@gmail.com>
2022-07-24 03:41:00 +0800

    :truck: Move middleware to src folder

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import type { NextFetchEvent, NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+export function middleware(_req: NextRequest, _ev: NextFetchEvent) {
+  const ContentSecurityPolicy = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.honghong.me data:;
+    style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+    img-src * blob: data:;
+    media-src;
+    connect-src *;
+    font-src 'self' fonts.gstatic.com;
+  `
+
+  const response = NextResponse.next()
+
+  response.headers.set(
+    'Content-Security-Policy',
+    ContentSecurityPolicy.replace(/\n/g, '')
+  )
+  response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin')
+  response.headers.set(
+    'Permissions-Policy',
+    'camera=(), microphone=(), geolocation=()'
+  )
+  response.headers.set(
+    'Strict-Transport-Security',
+    'max-age=31536000; includeSubDomains; preload'
+  )
+  response.headers.set('X-Frame-Options', 'DENY')
+  response.headers.set('X-Content-Type-Options', 'nosniff')
+  response.headers.set('X-DNS-Prefetch-Control', 'on')
+
+  return response
+}
