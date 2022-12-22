d06302b3747e5657847c58722a5241bd8bd8d89a
tszhong0411 <lainelson411@gmail.com>
2022-12-22 20:35:27 +0800

    feat: update logo

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,8 +1,14 @@
-import { Container, Group, Header as MantineHeader } from '@mantine/core'
+import {
+  Container,
+  Group,
+  Header as MantineHeader,
+  useMantineColorScheme,
+} from '@mantine/core'
 import React from 'react'
 
+import Logo from '@/components/Logo'
+
 import { useStyles } from './Header.styles'
-import HeaderLogo from './HeaderLogo'
 import LanguageSwitch from './LanguageSwitch'
 import ThemeSwitch from './ThemeSwitch'
 
@@ -10,12 +16,14 @@ export const HEADER_HEIGHT = 56
 
 export default function Header() {
   const { classes } = useStyles()
+  const { colorScheme } = useMantineColorScheme()
+  const dark = colorScheme === 'dark'
 
   return (
     <>
       <MantineHeader height={HEADER_HEIGHT} sx={{ position: 'relative' }}>
         <Container className={classes.inner}>
-          <HeaderLogo />
+          <Logo width={20} color={dark ? 'white' : 'black'} />
           <Group spacing={8} className={classes.social} position='right' noWrap>
             <LanguageSwitch />
             <ThemeSwitch />
