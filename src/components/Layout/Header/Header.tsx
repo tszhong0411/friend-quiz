8d107a46b8b96e8ba10479d5eb4bbd7222c2a5af
tszhong0411 <lainelson411@gmail.com>
2023-04-02 19:31:54 +0800

    fix: 減少頁首的寬度

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -5,7 +5,7 @@ import Logo from './Logo'
 const Header = () => {
   return (
     <header className='fixed top-0 left-0 right-0 z-40 shadow-sm backdrop-blur-[10px] bg-black/50 saturate-100'>
-      <div className='container mx-auto px-4 py-6 flex justify-between items-center'>
+      <div className='container mx-auto px-4 py-6 flex justify-between items-center max-w-4xl'>
         <Link href='/' className='flex items-center'>
           <Logo />
         </Link>
