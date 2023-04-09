c2ece4f5936e8668b0d7d33bfcfa41fc3b46ce12
tszhong0411 <lainelson411@gmail.com>
2023-04-10 03:59:57 +0800

    style: sort classname

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -4,8 +4,8 @@ import Logo from './Logo'
 
 const Header = () => {
   return (
-    <header className='fixed top-0 left-0 right-0 z-40 shadow-sm backdrop-blur-[10px] bg-black/50 saturate-100'>
-      <div className='container mx-auto px-4 py-6 flex justify-between items-center max-w-4xl'>
+    <header className='fixed left-0 right-0 top-0 z-40 bg-black/50 shadow-sm saturate-100 backdrop-blur-[10px]'>
+      <div className='container mx-auto flex max-w-4xl items-center justify-between px-4 py-6'>
         <Link href='/' className='flex items-center'>
           <Logo />
         </Link>
